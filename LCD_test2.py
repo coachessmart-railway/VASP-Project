f2d90dfa7a6cd4a1e136990119b575aab56025f1
Saniya Pathan <pathansaniya0103@gmail.com>
2026-06-17 18:30:49 +0530

    Updated for logic

--- LCD_test2.py
+++ LCD_test2.py
@@ -5,19 +5,22 @@ import time
 GPIO.setwarnings(False)
 GPIO.cleanup()
 
-time.sleep(1)
+print("Starting LCD Test...")
 
 lcd = CharLCD(
     numbering_mode=GPIO.BCM,
-    cols=20,
-    rows=4,
     pin_rs=26,
     pin_e=19,
-    pins_data=[13, 6, 5, 11]
+    pins_data=[13, 6, 5, 11],
+    cols=20,
+    rows=4
 )
 
 lcd.clear()
-lcd.write_string("HELLO SANIYA")
+
+lcd.write_string("HELLO")
+
+print("HELLO sent to LCD")
 
 while True:
     time.sleep(1)
